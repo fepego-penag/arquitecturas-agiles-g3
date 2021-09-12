6e82f761f0811ff3221cf803868c8ee75227b2bb
Jael Alexander Rivera Oviedo <jael.rivera@mercadolibre.com.co>
2021-09-12 03:42:26 -0500

    creation of monitor service

--- monitor/server.py
+++ monitor/server.py
@@ -1,18 +1,22 @@
 import schedule
 import threading, time
 import uuid
-import json
 from datetime import datetime
 
 
+from confluent_kafka import KafkaError
+from confluent_kafka import avro
+from confluent_kafka.avro import AvroProducer
+from confluent_kafka.avro import AvroConsumer
+from confluent_kafka.avro.serializer import SerializerError
+from confluent_kafka.cimpl import TopicPartition
 
-from kafka import KafkaConsumer, KafkaProducer
 
-topic_billing_control = "billing-control"
-topic_billing_health = "billing-health"
 
-topic_patient_control = "patient-control"
-topic_patient_health = "patient-health"
+topic_monito_control = "monitor_control"
+topic_microservices_healt = "microservices_health"
+boostrap_servers = 'localhost:9092'
+schema_registry_url = 'http://localhost:8081'
 
 class Producer(threading.Thread):
     def __init__(self):
@@ -31,23 +35,23 @@ class Producer(threading.Thread):
     
     def job(self):
 
-        message_to_billing = {
-                "microservice_name": "message_billing",
-                "timestamp": datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),
+        value_schema = avro.load('avro/ValueSchema.avsc')
+        key_schema = avro.load('avro/KeySchema.avsc')
+        avroProducer = AvroProducer(
+        {'bootstrap.servers': boostrap_servers, 'schema.registry.url': schema_registry_url},
+          default_key_schema=key_schema, default_value_schema=value_schema)
+          
+        key = str(uuid.uuid4())
+
+
+        message_to_microservice = {
+                "timestamp": str(datetime.now().strftime("%m/%d/%Y, %H:%M:%S")),
                 "transaction_id": str(uuid.uuid4())
             }
+       
+        avroProducer.produce(topic=topic_monito_control, value=message_to_microservice, key=key, key_schema=key_schema, value_schema=value_schema)
+        avroProducer.flush()
 
-        patient_to_billing = {
-                "microservice_name": "message_patient",
-                "timestamp": datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),
-                "transaction_id": str(uuid.uuid4())
-            }
-
-        producer = KafkaProducer(bootstrap_servers='localhost:29092', value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-        producer.send(topic_billing_control, json.dumps(message_to_billing))
-
-        producer.send(topic_patient_control, json.dumps(patient_to_billing))
-        producer.close()
 
 
 class Consumer(threading.Thread):
@@ -59,24 +63,39 @@ class Consumer(threading.Thread):
         self.stop_event.set()
 
     def run(self):
-        consumer = KafkaConsumer(bootstrap_servers='localhost:29092',
-                                 auto_offset_reset='earliest',
-                                 consumer_timeout_ms=1000)
-        consumer.subscribe([topic_billing_control, topic_patient_control])
+        c = AvroConsumer(
+        {'bootstrap.servers': boostrap_servers, 'group.id': 'monitor-2', 'schema.registry.url': schema_registry_url,
+        "api.version.request": True})
+        c.subscribe([topic_microservices_healt])
 
-        while not self.stop_event.is_set():
-            for message in consumer:
-                print(message)
-                if self.stop_event.is_set():
-                    break
-
-        consumer.close()
+        running = True
+        while running:
+            msg = None
+            try:
+                msg = c.poll(10)
+                if msg:
+                    if not msg.error():
+                        print(msg.value())
+                        print(msg.key())
+                        print(msg.partition())
+                        print(msg.offset())
+                        c.commit(msg)
+                    elif msg.error().code() != KafkaError._PARTITION_EOF:
+                        print(msg.error())
+                        running = False
+                else:
+                    print("No Message!! Happily trying again!!")
+            except SerializerError as e:
+                print("Message deserialization failed for %s: %s" % (msg, e))
+                running = False
+        c.commit()
+        c.close()
 
 
 def main():
     tasks = [
-        Producer(),
-        Consumer()
+        Producer()
+        
     ]
 
     for t in tasks:
